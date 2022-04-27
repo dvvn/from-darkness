a191e860a899e13a2df5d80148862ac0c36aa584
Иван <dvvn@ya.ru>
2022-04-27 17:21:32 +0300

    sync

--- impl/cheat/csgo/interfaces/BaseClient.cpp
+++ impl/cheat/csgo/interfaces/BaseClient.cpp
@@ -18,6 +18,6 @@ CHEAT_CSGO_INTERFACE_INIT(IBaseClientDLL)
 bool IBaseClientDLL::DispatchUserMessage(int msg_type, int flags, int size, const void* msg)
 {
 	//return dhooks::invoke(&IBaseClientDLL::DispatchUserMessage, static_cast<size_t>(38), this, msg_type, flags, size, msg);
-	const decltype(&IBaseClientDLL::DispatchUserMessage) fn = nstd::basic_address(this).deref<1>( )[38];
+	const decltype(&IBaseClientDLL::DispatchUserMessage) fn = nstd::mem::basic_address(this).deref<1>( )[38];
 	return std::invoke(fn, this, msg_type, flags, size, msg);
 }
