1cfcf7c651af1fc9ca563a538f50dc02d259cbcd
Иван <dvvn@ya.ru>
2022-06-03 21:23:33 +0300

    cleanup

--- impl/cheat/hooks/impl.h
+++ impl/cheat/hooks/impl.h
@@ -15,41 +15,6 @@ import cheat.hook;
 #define IS_STATIC_member false
 #define IS_STATIC_static true
 
-// clang-format off
-#define CONCAT_NAMESPACE(_L_, _R_) #_L_"::"#_R_
-// clang-format on
-
-#define CHEAT_HOOK_BEGIN(_HOOK_SOURCE_, _HOOK_NAME_, _FN_TYPE_, _FN_RET_, ...)            \
-    using namespace cheat;                                                                \
-    using namespace hooks;                                                                \
-    using namespace _HOOK_SOURCE_;                                                        \
-    struct _HOOK_NAME_##_impl final : hook, hook_instance_##_FN_TYPE_<_HOOK_NAME_##_impl> \
-    {                                                                                     \
-        bool is_static() const override                                                   \
-        {                                                                                 \
-            return IS_STATIC_##_FN_TYPE_;                                                 \
-        }                                                                                 \
-        std::string_view name() const override                                            \
-        {                                                                                 \
-            return CONCAT_NAMESPACE(_HOOK_SOURCE_, _HOOK_NAME_);                          \
-        }                                                                                 \
-        void init() override;                                                             \
-        FN_##_FN_TYPE_ _FN_RET_ callback(__VA_ARGS__);
-
-#define CHEAT_HOOK_END(_HOOK_NAME_)                        \
-    }                                                      \
-    ; /*_HOOK_NAME_ -> one_instance_t::operator size_t()*/ \
-    CHEAT_OBJECT_BIND(base, _HOOK_NAME_, _HOOK_NAME_##_impl)
-
-#define CHEAT_HOOK_BODY(_HOOK_SOURCE_, _HOOK_NAME_, _FN_TYPE_, _FN_RET_, ...)      \
-    CHEAT_HOOK_BEGIN(_HOOK_SOURCE_, _HOOK_NAME_, _FN_TYPE_, _FN_RET_, __VA_ARGS__) \
-    CHEAT_HOOK_END(_HOOK_NAME_)
-
-#define CHEAT_HOOK_INIT(_HOOK_NAME_) void _HOOK_NAME_##_impl::init()
-#define CHEAT_HOOK_CALLBACK(_HOOK_NAME_, _FN_RET_, ...) _FN_RET_ _HOOK_NAME_##_impl::callback(__VA_ARGS__)
-
-//---
-
 template <class T>
 concept have_find_method = requires
 {
@@ -135,7 +100,7 @@ class hook_index_extractor
         return std::pair(start, end);
     }
 
-    constexpr hook_index_extractor(const std::string_view file_name, const bool validate = false)
+    constexpr hook_index_extractor(const std::string_view file_name, const bool validate = true)
     {
         const auto [start, end] = find(file_name);
         if (validate && !_Extractor_validate(file_name, start, end))
--- impl/cheat/hooks/interface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cheat/core/object.h>
-
-import cheat.hooks.base;
-
-#define CHEAT_HOOK(_NAME_, _IDX_) CHEAT_OBJECT(_NAME_, base, _IDX_)
