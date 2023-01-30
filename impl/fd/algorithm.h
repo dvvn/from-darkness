c5f861e831a820e82b49b560c8cc7fe2ced7dee7
dvvn <dvvn@ya.ru>
2023-01-30 20:32:18 +0100

    dummy

--- impl/fd/algorithm.h
+++ impl/fd/algorithm.h
@@ -16,10 +16,46 @@ void iterate(It begin, It end, Fn fn)
 template <native_iterable T, typename Fn>
 void iterate(T&& container, Fn fn)
 {
-    auto last = end(container);
-    for (auto itr = begin(container); itr != last; ++itr)
+    //todo : pass fn by reference if not trivially copyable]
+    iterate(begin(container), end(container), fn);
+}
+#if 0
+template<typename Src, typename Dst>
+concept can_memmove = std::is_trivialy_destructible_v<> && std::convertible_to<Src, const void*> && std::convertible_to<Dst, void*>;
+
+template<typename Src, typename Dst>
+constexpr void copy(Src begin, Src end, Dst dst)
+{
+    if constexpr(can_memmove<Src, Dst>)
     {
-        fn(*itr);
+        if(!std::is_constant_evaluated())
+        {
+#ifdef _DEBUG
+            if(begin == end)
+                return;
+#endif
+            memmove(dst, begin, std::distance(begin, end) * sizeof(std::iter_value_t<Src>));
+            return;
+        }
     }
+
+    while(begin != end)
+    {
+        *dst++ = *begin++;
+    }
+}
+#endif
+
+template<typename Src, typename Dst>
+constexpr void copy(Src begin, size_t count, Dst dst)
+{
+    //todo: pass iterator by ref if not trivially copyable
+    return copy(begin, begin + size, dst);
+}
+
+template<native_iterable T,typename Dst>
+void copy(T&& container, Dst dst)
+{
+    copy(begin(container), end(container), dst);
 }
 }
