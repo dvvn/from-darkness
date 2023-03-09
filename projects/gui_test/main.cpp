626eef32c80e5fa68ef1c6dae94401889893a577
Иван <dvvn@ya.ru>
2023-03-09 21:19:54 +0100

    sync

--- projects/gui_test/main.cpp
+++ projects/gui_test/main.cpp
@@ -4,12 +4,13 @@
 #include <fd/gui/menu.h>
 #include <fd/hooking/callback.h>
 #include <fd/hooking/storage.h>
+#include <fd/utils/functional.h>
 
 #include <imgui.h>
 
 #include <spdlog/spdlog.h>
 
-int main(int, char**)
+int main(int, char **)
 {
     using namespace fd;
     backend_data backend;
@@ -21,39 +22,35 @@ int main(int, char**)
     spdlog::set_level(spdlog::level::debug);
 
 #if defined(_DEBUG) && 0
-    const default_assert_handler assertHandler([&](assert_data const& adata) { spdlog::critical(parse(adata)); });
+    const default_assert_handler assertHandler([&](assert_data const &adata) { spdlog::critical(parse(adata)); });
 #endif
 
     //----
 
-    auto testMenu = menu(
+    auto test_menu = menu(
         tab_bar(
             tab("tab1", [] { ImGui::TextUnformatted("hello"); }),
             tab("tab2", [] { ImGui::TextUnformatted("-->hello again"); })),
-        tab_bar(
-            tab("new tab", [] { ImGui::TextUnformatted("im here!"); }),
-            tab("tab 3", [] { ImGui::TextUnformatted("yes!"); })));
-    auto guiCtx = gui_context(
-        { false, backend.d3d, backend.hwnd },
-        [&]
-        {
-            testMenu.render();
+        tab_bar(tab("new tab", [] { ImGui::TextUnformatted("im here!"); }), tab("tab 3", [] {
+                    ImGui::TextUnformatted("yes!");
+                })));
+    auto gui_ctx = gui_context([&] {
+        test_menu.render();
 #ifndef IMGUI_DISABLE_DEMO_WINDOWS
-            ImGui::ShowDemoWindow();
+        ImGui::ShowDemoWindow();
 #endif
-        });
+    });
 
-    if (!guiCtx)
+    if (!gui_ctx.init({ false, backend.d3d, backend.hwnd }))
         return FALSE;
 
     auto hooks = hooks_storage(
         hook_callback_args(
             "WinAPI.WndProc",
             backend.info.lpfnWndProc,
-            [&](auto orig, auto... args) -> LRESULT
-            {
+            [&](auto orig, auto... args) -> LRESULT {
                 using keys_return = basic_gui_context::keys_return;
-                switch (guiCtx.process_keys(args...))
+                switch (gui_ctx.process_keys(args...))
                 {
                 case keys_return::instant:
                     return TRUE;
@@ -68,23 +65,17 @@ int main(int, char**)
         hook_callback_args(
             "IDirect3DDevice9::Reset",
             fn_sample(&IDirect3DDevice9::Reset, vfunc(backend.d3d, 16)),
-            [&](auto orig, auto, auto... args)
-            {
-                guiCtx.release_textures();
+            [&](auto orig, auto, auto... args) {
+                gui_ctx.release_textures();
                 return orig(args...);
             }),
         hook_callback_args(
             "IDirect3DDevice9::Present",
             fn_sample(&IDirect3DDevice9::Present, vfunc(backend.d3d, 17)),
-            [&](auto orig, auto thisPtr, auto... args)
-            {
-                guiCtx.render(thisPtr);
+            [&](auto orig, auto this_ptr, auto... args) {
+                gui_ctx.render(this_ptr);
                 return orig(args...);
             }));
 
-    if (!hooks.enable())
-        return EXIT_FAILURE;
-
-    backend.run();
-    return EXIT_SUCCESS;
+    return hooks.enable() && backend.run() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
